222e69be464a3096fc7c08c16333ca742c9e4938
vsmolina <molinav2000@gmail.com>
2022-02-08 19:40:17 -0600

    deploy

---
+++ store/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import {Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <Routes>
+        <Route exact path="/" element={<ProductList/>} />
+        <Route path="/details" element={<Details/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="*" element={<Default/>} />
+      </Routes>
+      <Modal />
+    </React.Fragment>
+  );
+}
+
+export default App;
